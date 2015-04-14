08d96590234596a205db635f1eb188fb8a942733
sylver-john <sylverimhoff@gmail.com>
2015-04-14 12:31:47 +0200

    PingPoler.go

--- PingPoller.go
+++ PingPoller.go
@@ -1,29 +1,47 @@
 package main
 
 import "golang.org/x/net/icmp"
-import "net"
 import "os"
 import "fmt"
+import "net"
 
 
 type Destination struct{
 	url string
 }
 
-func () String() {
-	
-}
+
 
 func Ping(s string) int{
-	var packetConn *icmp.PacketConn
+	//var packetConn *icmp.PacketConn
+	var errorCode int
 	var data []byte
-	var dst net.Addr = s
-	packetConn = icmp.ListenPacket("ip4:icmp","")
-	duration,err = packetConn.WriteTo(data,dst)
-	return duration
+	ip,err := net.ResolveIPAddr("ip",s)
+	if(err==nil){
+		var dst net.Addr = ip
+		packetConn, err := icmp.ListenPacket("ip4:icmp","")
+		if(err==nil){
+			errorCode,err := packetConn.WriteTo(data,dst)
+			return errorCode
+			if(err!=nil){
+				fmt.Println("error on writeTo")
+			}
+		}else{
+			fmt.Println("error on ListenPacket")
+		}
+	}else{
+		errorCode := -1
+		return errorCode
+	}
+	return errorCode
 }
 
 func main() {
-	duration :=Ping(os.Args[1])
-	fmt.Println(duration)
+	fmt.Println("Ping on : " + os.Args[1])
+	errorCode :=Ping(os.Args[1])
+	if(errorCode==0){
+		fmt.Println("It works !")
+	}else{
+		fmt.Println("Ping failed....")
+	}
 }
