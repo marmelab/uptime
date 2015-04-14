cd0b93fdb11406f7b2827cfdf61b69e83775211d
sylver-john <sylverimhoff@gmail.com>
2015-04-14 11:04:58 +0200

    PingPoller.go

--- PingPoller.go
+++ PingPoller.go
@@ -2,12 +2,28 @@ package main
 
 import "golang.org/x/net/icmp"
 import "net"
+import "os"
+import "fmt"
 
 
 type Destination struct{
 	url string
 }
 
-func Ping(s string) {
+func () String() {
 	
+}
+
+func Ping(s string) int{
+	var packetConn *icmp.PacketConn
+	var data []byte
+	var dst net.Addr = s
+	packetConn = icmp.ListenPacket("ip4:icmp","")
+	duration,err = packetConn.WriteTo(data,dst)
+	return duration
+}
+
+func main() {
+	duration :=Ping(os.Args[1])
+	fmt.Println(duration)
 }
