64bf138e9ef4daad4ae14cd0a53fcde4b0727492
sylver-john <sylverimhoff@gmail.com>
2015-04-15 17:29:00 +0200

    add PingPoller_test.go
    add Json read
    
    display JSON results in console
    
    add test Ping
    
    update test ping
    
    gofmt
    
    add api.go
    
    docker-compose remove nginx
    
    update main PingPoller and ips
    
    add api.go
    
    docker-compose remove nginx
    
    update api : display list of ip as JSON format by the url localhost:8000/ips/
    
     update main.go and add model.go
    
     add model and POST on apo
    
     add apiPoller and update Post on main
    
     rm apiPoller file
    
     update api Post method
    
    rm comment from api.go
    
    add test for Post request on api
    
     update main.go
    
    add travis.yml
    
    add config.trevis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
    update pingpoller test
    
    update all
    
    add travis.yml
    
    update all main and pingpoller
    
    update all main and pingpoller
    
    main.go and gofmt
    
    update branch
    
     remove makefile
    
     travis.yml
    
     test failed
    
     .travis.yml and test
    
    add api.go
    
    docker-compose remove nginx
    
    update api : display list of ip as JSON format by the url localhost:8000/ips/
    
     update main.go and add model.go
    
     add model and POST on apo
    
     add apiPoller and update Post on main
    
     rm apiPoller file
    
     update api Post method
    
    rm comment from api.go
    
    add test for Post request on api
    
     update main.go
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
    update pingpoller test
    
    update pingpoller test
    
    add import fmt in main
    
    rebase all
    
    add api.go
    
    add api.go
    
    update api : display list of ip as JSON format by the url localhost:8000/ips/
    
     update main.go and add model.go
    
     add model and POST on apo
    
     add apiPoller and update Post on main
    
     rm apiPoller file
    
     update api Post method
    
    rm comment from api.go
    
    add test for Post request on api
    
     update main.go
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
    update pingpoller test
    
    update pingpoller test
    
    update pingpoller test
    
    add api.go
    
    update api : display list of ip as JSON format by the url localhost:8000/ips/
    
    add api.go
    
    docker-compose remove nginx
    
     update main.go and add model.go
    
     add model and POST on apo
    
     add apiPoller and update Post on main
    
     rm apiPoller file
    
     update api Post method
    
    rm comment from api.go
    
    add test for Post request on api
    
     update main.go
    
    add travis.yml
    
    add config.trevis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
     add .travis.yml
    
    update pingpoller test
    
    update pingpoller test
    
    update pingpoller test
    
    repair main and api
    
     add feedline at travis.yml and model
    
    add url in url.json and retrieve url from it in main.go
    
     add channel in main.go for retrieve ip adress

---
+++ app/api/model/model.go
@@ -0,0 +1,10 @@
+
+package model
+
+
+type Ip struct {
+	Destination string `json:"destination"`
+}
+
+type Ips []Ip
+
--- app/main.go
+++ app/main.go
@@ -1,72 +1,76 @@
 package main
 
 import (
+	"./api/model"
 	"./poller"
+	"encoding/json"
 	"fmt"
-	"net"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"time"
 )
 
 func main() {
 	response := poller.Response{}
-	var ip *net.IPAddr
-	var url string
-	var err error
+	listOfDestination := model.Ips{}
 	var duration int
+	var url string
 	for true {
-		listOfUrl := poller.RetrieveIpsFromJsonFile("url.json")
-		listOfIp := poller.RetrieveIpsFromJsonFile(listOfUrl["urlIps"])
-		c := make(chan string, len(listOfIp))
-		go func() {
-			for _, value := range listOfIp {
-				c <- value
+		res, err := http.Get(poller.RetrieveIpsFromJsonFile("url.json")["urlApiIp"])
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			body, err := ioutil.ReadAll(res.Body)
+			res.Body.Close()
+			if err != nil {
+				log.Fatal(err)
 			}
-		}()
-		for range listOfIp {
-			url = <-c
-			ip, err = poller.FromDomainNameToIp(url)
-			response.Destination = url
-			if err == nil {
-				duration, err = poller.Ping(ip)
-				response.Time = duration
-				response.Error = err
-				if (err != nil) || (duration <= 0) {
-					response.Status = "failed"
-					fmt.Println(response)
-				} else {
-					response.Status = "good"
-					fmt.Println(response)
+			err = json.Unmarshal(body, &listOfDestination)
+			if err != nil {
+				log.Fatal(err)
+			}
+			c := make(chan string,len(listOfDestination))
+			go func(){
+				for i := 0; i < len(listOfDestination); i++ {
+					c <- listOfDestination[i].Destination
 				}
-			} else {
-				response.Status = "failed"
-				response.Time = duration
-				response.Error = err
-				fmt.Println(response)
+				}()
+			for i := 0; i < len(listOfDestination); i++ {
+				url = <-c
+				ip, err := poller.FromDomainNameToIp(url)
+				response.Destination = listOfDestination[i].Destination
+				if err == nil {
+					duration, err = poller.Ping(ip)
+					response.Time = duration
+					response.Error = err
+					if (err != nil) || (duration <= 0) {
+						response.Status = "failed"
+						err = poller.DoPostOn(&response, poller.RetrieveIpsFromJsonFile("url.json")["urlApiResults"])
+						if err != nil {
+							log.Print(err)
+						}
+					} else {
+						response.Status = "good"
+						err = poller.DoPostOn(&response, poller.RetrieveIpsFromJsonFile("url.json")["urlApiResults"])
+						if err != nil {
+							log.Print(err)
+						}
+					}
+				} else {
+					response.Status = "failed"
+					response.Time = duration
+					response.Error = err
+					err = poller.DoPostOn(&response, poller.RetrieveIpsFromJsonFile("url.json")["urlApiResults"])
+					if err != nil {
+						log.Print(err)
+					}
+				}
+
 			}
 		}
-		/*fr key, value := range listOfIp {
-			ip, err = poller.FromDomainNameToIp(value)
-			response.Destination = value
-			response.Key = key
-			if err == nil {
-				duration, err = poller.Ping(ip)
-				response.Time = duration
-				response.Error = err
-				if (err != nil) || (duration <= 0) {
-					response.Status = "failed"
-					fmt.Println(response)
-				} else {
-					response.Status = "good"
-					fmt.Println(response)
-				}
-			} else {
-				response.Status = "failed"
-				response.Time = duration
-				response.Error = err
-				fmt.Println(response)
-			}
-		}*/
 		time.Sleep(time.Second * 10)
 		fmt.Println("===============================")
 	}
+i
 }
