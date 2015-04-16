fbec3f14412aa4a2540b574abc230feffcb08f30
sylver-john <sylverimhoff@gmail.com>
2015-04-16 17:29:56 +0200

    update api : display list of ip as JSON format by the url localhost:8000/ips/

--- app/api/api.go
+++ app/api/api.go
@@ -1,17 +1,31 @@
 package main
 
 import (
-    "fmt"
-    "html"
     "log"
     "net/http"
+    "encoding/json"
+    "fmt"
+
+
 )
 
+type Ip struct{
+	Destination string   `json:"destination"`
+}
+
+type Ips []Ip
+
+
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+    http.HandleFunc("/ips/", func(w http.ResponseWriter, r *http.Request) {
+
+        listIp := Ips{
+        	Ip{Destination: "google.fr"},
+        	Ip{Destination: "youtube.fr"},
+        	Ip{Destination: "bing.fr"},
+        	Ip{Destination: "szszdzdadafdff.fr"},
+        }
+        json.NewEncoder(w).Encode(listIp)
     })
-
     log.Fatal(http.ListenAndServe(":8000", nil))
-
 }
---
+++ app/api/model.go
@@ -0,0 +1,7 @@
+package main 
+
+type ip struct{
+	destination string
+}
+
+type ips []ip
