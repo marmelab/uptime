9e2912051b62cf191ebb89fdb3ef0ba6a186b4e0
sylver-john <sylverimhoff@gmail.com>
2015-04-16 12:00:25 +0200

    update test ping

--- app/poller/PingPoller_test.go
+++ app/poller/PingPoller_test.go
@@ -17,13 +17,25 @@ func TestPingValidDestination(t *testing.T) {
 }
 
 func TestPingWrongDestination(t *testing.T) {
-	ip, err := net.ResolveIPAddr("ip", "localhostzfedffs")
+	ip, err := net.ResolveIPAddr("ip", "localhost?Ithasnosense")
 	if(err == nil){
 		duration,error :=Ping(ip)
 		if(error != nil){
 			t.Error("Expected error, got", err)
 		}else if(duration <= 0){
 			t.Error("Expected duration < 0, got", duration)
+		}	
+	}
+}
+
+func TestPingNoDestination(t *testing.T) {
+	ip, err := net.ResolveIPAddr("ip", "")
+	if(err == nil){
+		duration,error :=Ping(ip)
+		if(error != nil){
+			t.Error("Expected no error, got", err)
+		}else if(duration <= 0){
+			t.Error("Expected duration > 0, got", duration)
 		}	
 	}
 }
