f87c5f9d28ceffe79712ca649414afeb553f0f84
sylver-john <sylverimhoff@gmail.com>
2015-04-20 09:58:29 +0200

    add test for Post request on api

--- app/poller/ApiPoller.go
+++ app/poller/ApiPoller.go
@@ -2,21 +2,21 @@ package poller
 
 import (
 	"bytes"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func DoPostOn(response *Response, url string) {
+func DoPostOn(response *Response, url string)error{
 	var result = []byte(`{"Destination": "` + response.Destination + `","Time": ` + strconv.Itoa(response.Time) + `,"Status": "` + response.Status + `"}`)
 	req, error := http.NewRequest("POST", url, bytes.NewBuffer(result))
 	if error != nil {
-		log.Fatal(error)
+		return error
 	}
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer resp.Body.Close()
+	return err
 }
