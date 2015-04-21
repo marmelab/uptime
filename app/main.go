5484d25d788eef217ac6a3937cc5ad67cd27ebd2
sylver-john <sylverimhoff@gmail.com>
2015-04-21 10:11:31 +0200

    update branch

--- app/main.go
+++ app/main.go
@@ -10,13 +10,41 @@ import (
 func main() {
 	response := poller.Response{}
 	var ip *net.IPAddr
+	var url string
 	var err error
 	var duration int
-	//c := make(chan string)
 	for true {
 		listOfUrl := poller.RetrieveIpsFromJsonFile("url.json")
 		listOfIp := poller.RetrieveIpsFromJsonFile(listOfUrl["urlIps"])
-		for key, value := range listOfIp {
+		c := make(chan string, len(listOfIp))
+		go func() {
+			for _, value := range listOfIp {
+				c <- value
+			}
+		}()
+		for range listOfIp {
+			url = <-c
+			ip, err = poller.FromDomainNameToIp(url)
+			response.Destination = url
+			if err == nil {
+				duration, err = poller.Ping(ip)
+				response.Time = duration
+				response.Error = err
+				if (err != nil) || (duration <= 0) {
+					response.Status = "failed"
+					fmt.Println(response)
+				} else {
+					response.Status = "good"
+					fmt.Println(response)
+				}
+			} else {
+				response.Status = "failed"
+				response.Time = duration
+				response.Error = err
+				fmt.Println(response)
+			}
+		}
+		/*fr key, value := range listOfIp {
 			ip, err = poller.FromDomainNameToIp(value)
 			response.Destination = value
 			response.Key = key
@@ -37,8 +65,8 @@ func main() {
 				response.Error = err
 				fmt.Println(response)
 			}
-		}
-		time.Sleep(10000000000)
+		}*/
+		time.Sleep(time.Second * 10)
 		fmt.Println("===============================")
 	}
 }
