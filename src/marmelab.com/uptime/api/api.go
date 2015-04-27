03333b7d498683f30e3155c620ea459589a8b085
sylver-john <sylverimhoff@gmail.com>
2015-04-27 16:29:38 +0200

    add driver and lib for postgreSql

--- src/marmelab.com/uptime/api/api.go
+++ src/marmelab.com/uptime/api/api.go
@@ -7,12 +7,17 @@ import (
 	"log"
 	"net/http"
 	"flag"
+	_"github.comlib/pq"
+	"database/sql"
 )
 
 func main() {
-
 	port := flag.String("port","8000","port for the api listen")
 	flag.Parse()
+	db,err := sql.Open("postgres","user=postgres dbname=uptime sslmode=verify-full")
+	if(err!=nil){
+		log.Fatal("error open db")
+	}
 	http.HandleFunc("/ips/", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != "GET"{
 			w.Header().Set("Statuscode","404")
