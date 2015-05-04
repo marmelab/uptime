c866035fe6a2b4a7359e7f3d643177ec0c683c10
sylver-john <sylverimhoff@gmail.com>
2015-05-04 15:47:49 +0200

    webpack-dev-serv

--- front/src/helloworld.js
+++ front/src/helloworld.js
@@ -1,8 +1,8 @@
 var HelloWorld = React.createClass({
-  displayName: 'HelloWorld',
+  displayName: 'Helloworld !',
   render: function() {
     return (
-      <h1>Helloworld!</h1>
+      <h1>It works!</h1>
     );
   }
 });
