d11dd2bda057614ece9271d74c4d9aeecdfd30dc
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2015-05-07 12:02:13 +0200

    Add React Hot Loader

--- front/src/helloworld.js
+++ front/src/helloworld.js
@@ -1,11 +1,11 @@
+var React = require('react');
+
 var HelloWorld = React.createClass({
   render: function() {
     return (
-      <h1>It works yeahhh!</h1>
+        <h1>Hello {this.props.name}!!!</h1>
     );
   }
 });
-React.render(
-  React.createElement(HelloWorld, null),
-  document.getElementById('content')
-);
+
+module.exports = HelloWorld;
