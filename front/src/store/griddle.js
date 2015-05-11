3549bf49182435ff88fb4e183a85a379c30dd494
sylver-john <sylverimhoff@gmail.com>
2015-05-11 12:04:37 +0200

    create store, component and action repo

---
+++ front/src/store/griddle.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TargetDatagrid from '../component/TargetDatagrid.js';
+
+var data = [
+	{
+		"id":"1",
+		"destination":'google.fr',
+		"date":"15/05/15",
+		"status":"good",
+		"time":240
+	},
+	{
+		"id":"2",
+		"destination":'rhfjfj',
+		"date":"15/05/15",
+		"status":"failed",
+		"time":240
+	}
+];
+
+module.exports = TargetDatagrid;
+ 
