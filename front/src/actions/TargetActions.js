709dba17c979d647e982db84aff9520bb650fe37
sylver-john <sylverimhoff@gmail.com>
2015-05-20 17:26:20 +0200

    using fecth instead of ajax in TargetActions

--- front/src/actions/TargetActions.js
+++ front/src/actions/TargetActions.js
@@ -11,12 +11,25 @@ class TargetActions {
 
 	showResults() {
 		this.dispatch("SHOW_RESULTS");
-
+		var url = API_BASE_URL + "/ips/results"
 		this.actions.setLoading(true);
 		this.actions.setError(false);
 
-		$.ajax({
-			url: API_BASE_URL + "/ips/",
+		fetch(url)
+		.then(function(response) {
+			this.actions.setLoading(false);
+			this.actions.setResults(response);
+			this.actions.setError(false);
+		}.bind(this))
+		.catch(function() {
+			this.actions.setLoading(false);
+			this.actions.setError(true);
+		}.bind(this));
+
+
+		/*$.ajax({
+			url: API_BASE_URL + "/ips/results",
+>>>>>>>  using fecth instead of ajax in TargetActions
 			complete: function() {
 				this.actions.setLoading(false);
 			}.bind(this),
@@ -27,7 +40,7 @@ class TargetActions {
 			error: function(error){
 				this.actions.setError(true);
 			}.bind(this)
-		});
+		});*/
 	}
 }
 
