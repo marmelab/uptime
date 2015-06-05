46f5c6e2a654640a46b690cff706db819e2e83f4
sylver-john <sylverimhoff@gmail.com>
2015-06-05 15:25:24 +0200

    resultschart indentation

--- front/src/components/ResultDurationChart.js
+++ front/src/components/ResultDurationChart.js
@@ -3,22 +3,22 @@ import TargetStore from '../stores/TargetStore';
 import TargetAction from '../actions/TargetActions';
 
 class ResultDurationChart extends LineChart {
-  constructor(props) {
-    super(props);
-  }
+ 	constructor(props) {
+		super(props);
+	}
 
-  componentDidMount() {
-    TargetAction.fetchResults();
-    TargetStore.addChangeListener(this.onChange.bind(this));
-  }
+	componentDidMount() {
+		TargetAction.fetchResults();
+		TargetStore.addChangeListener(this.onChange.bind(this));
+	}
 
-  componentWillUnmount() {
-    super.componentWillUnmount();
-    TargetStore.removeChangeListener(this.onChange.bind(this));
-  }
-  onChange() {
-    this.setState(TargetStore.getResults());
-  }
+	componentWillUnmount() {
+		super.componentWillUnmount();
+		TargetStore.removeChangeListener(this.onChange.bind(this));
+	}
+	onChange() {
+		this.setState(TargetStore.getResults());
+	}
 }
 
 export default ResultDurationChart;
