96e34024448f3f8fce1c11de6540298615173da8
sylver-john <sylverimhoff@gmail.com>
2015-06-25 09:05:56 +0200

    make error handler

--- src/marmelab.com/uptime/api/handlers/Targets.go
+++ src/marmelab.com/uptime/api/handlers/Targets.go
@@ -5,22 +5,10 @@ import (
 	"../target"
 	"encoding/json"
 	"github.com/gorilla/mux"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func SetCors(w *http.Header) {
-	w.Set("Access-Control-Allow-Origin", "*")
-	w.Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-}
-
-func error500(w http.ResponseWriter, err error, message string) {
-	log.Print(message, err)
-	http.Error(w, http.StatusText(500), 500)
-	return
-}
 
 func GetTargets(w http.ResponseWriter, r *http.Request) {
 	header := w.Header()
---
+++ src/marmelab.com/uptime/api/handlers/errors.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+func SetCors(w *http.Header) {
+	w.Set("Access-Control-Allow-Origin", "*")
+	w.Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
+func error500(w http.ResponseWriter, err error, message string) {
+	log.Print(message, err)
+	http.Error(w, http.StatusText(500), 500)
+	return
+}
